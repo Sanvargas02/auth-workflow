d5929b41b023e870a3053df6a89e5ad5405ab10b
Santiago Vargas N <90786097+Sanvargas02@users.noreply.github.com>
2024-09-11 18:25:59 -0500

    UI of Login Component

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,14 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
   ],
-  template: `<p>login works!</p>`,
+  templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LoginComponent { }
+export default class LoginComponent {
+
+  showPassword = false;
+  email = '';
+  password = '';
+
+
+  constructor(private router: Router) {}
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login() {
+    // Aquí se integraría la lógica de autenticación, como llamar al servicio de login
+    console.log('Logging in with', this.email, this.password);
+    this.router.navigate(['/dashboard']);
+  }
+
+}
